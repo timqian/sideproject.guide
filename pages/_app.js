3e73022010881e6fa94e3acf2a34934688a0818f
Tim Qian <timqian92@qq.com>
2022-05-11 07:35:14 +0000

    Update content

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <div>
-    <Component {...pageProps} />
-    <div class="addthis_inline_follow_toolbox"></div>
-    <Script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-627b4d73b9595f5c"></Script>
-    </div>
-    
+  return <Component {...pageProps} />
 }
