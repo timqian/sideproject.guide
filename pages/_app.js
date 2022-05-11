0ab047b1d4e19c36ecf631426f9d048c9939f67e
Tim Qian <timqian92@qq.com>
2022-05-11 14:29:06 +0800

    test script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Component {...pageProps} />
+    <div class="addthis_inline_follow_toolbox"></div>
+    <Script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-627b4d73b9595f5c"></Script>
+    </div>
+    
 }
