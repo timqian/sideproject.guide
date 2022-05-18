b7d351b29e066a43b8875d2dd7a7a23e8263f88e
Tim Qian <timqian92@qq.com>
2022-05-18 11:44:51 +0800

    Update imgs

--- theme.config.js
+++ theme.config.js
@@ -16,45 +16,27 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Side Project Guide" />
+      <meta name="og:description" content="Side Project Guide" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://sideproject.guide/og.png" />
+      <meta name="twitter:site:domain" content="sideproject.guide" />
+      <meta name="twitter:url" content="https://sideproject.guide" />
+      <meta name="og:title" content="Side Project Guide" />
+      <meta name="og:image" content="https://sideproject.guide/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Side Project Guide" />
       <link
         rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
+        sizes="256x256"
+        href="/favicon.png"
       />
       <link
         rel="icon"
         type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
+        sizes="256x256"
+        href="/favicon.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/og.png" />
     </>
   ),
   search: true,
