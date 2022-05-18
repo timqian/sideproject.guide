c02f33d415d37970703b507bcf154c6ab58a0ecd
Tim Qian <timqian92@qq.com>
2022-05-18 12:28:24 +0800

    Update description

--- theme.config.js
+++ theme.config.js
@@ -16,14 +16,14 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Side Project Guide" />
-      <meta name="og:description" content="Side Project Guide" />
+      <meta name="description" content="如何提升 Side Project 的存活概率" />
+      <meta name="og:description" content="如何提升 Side Project 的存活概率" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://sideproject.guide/og.png" />
+      <meta name="twitter:image" content="https://sideproject.guide/preview.png" />
       <meta name="twitter:site:domain" content="sideproject.guide" />
       <meta name="twitter:url" content="https://sideproject.guide" />
       <meta name="og:title" content="Side Project Guide" />
-      <meta name="og:image" content="https://sideproject.guide/og.png" />
+      <meta name="og:image" content="https://sideproject.guide/preview.png" />
       <meta name="apple-mobile-web-app-title" content="Side Project Guide" />
       <link
         rel="apple-touch-icon"
